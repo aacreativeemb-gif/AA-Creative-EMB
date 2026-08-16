94536c7fd32db0e109c6d3af9a51d301a25f73a8
aacreativeemb-gif <aacreativeemb@gmail.com>
2026-08-17 02:40:08 +0500

    Add files via upload

--- android/app/src/main/java/com/aacreativeemb/support/ui/screens/HomeScreen.kt
+++ android/app/src/main/java/com/aacreativeemb/support/ui/screens/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ConfirmationNumber
 import androidx.compose.material.icons.filled.Forum
